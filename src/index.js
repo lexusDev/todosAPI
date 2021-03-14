75e3cde7b801a84a92d9204def0a9ff1b366c1cc
Alex Monteiro <alex.monteiro@digiage.com.br>
2021-03-13 21:59:54 -0300

    updating dependencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
-
 const app = express();
 
 app.use(cors());
